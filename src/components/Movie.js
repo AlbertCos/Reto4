92c3cf9fc8aa2f4d82063b9cae88267178308552
AlbertCos <cosmenalberto@gmail.com>
2021-02-16 18:31:49 +0100

    Finish

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
-const Movie=({title, poster_path, overview, vote_average}) =>(
+const Movie=({title, original_title, poster_path, overview,release_date, vote_average}) =>(
     <div className="movie">
         <img src={IMG_API + poster_path} alt={title} />
             <div className="movie-info">
@@ -10,7 +10,10 @@ const Movie=({title, poster_path, overview, vote_average}) =>(
                 <span>{vote_average}</span>
             </div>
             <div className="movie-over">
-                <h2>Overview: </h2>
+                <h3>{title}</h3>
+                <hr/>
+                <h4>{release_date} - {original_title}  </h4>
+                <h3>Overview: </h3>
                 <p>{overview}</p>
             </div>
     </div>
