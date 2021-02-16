b430bb0b1af89d61f70300fd86f644b92729b74a
AlbertCos <cosmenalberto@gmail.com>
2021-02-16 13:33:29 +0100

    Brke

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Movie=() =>(
     <div className="movie">
-
+        This is a movie component
     </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -11,7 +11,12 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+// Clave de la API (v3 auth)
+// 1cb22661a56a50da9fff9da1507d7089
+// Ejemplo de solicitud de API
+// https://api.themoviedb.org/3/movie/550?api_key=1cb22661a56a50da9fff9da1507d7089
+// Token de acceso de lectura a la API (v4 auth)
+// eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxY2IyMjY2MWE1NmE1MGRhOWZmZjlkYTE1MDdkNzA4OSIsInN1YiI6IjYwMmJiMDlkNjNkOTM3MDAzZGNlMjI2NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yuhVwm_pdR-pTydksS6-RAON47fev522K0-v90fzW0w
+
 reportWebVitals();
